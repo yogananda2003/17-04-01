770232f8762e54dcfab902c52e614886547e9b52
yogananda.b <byogananda6346@gmail.com>
2026-04-13 12:25:36 +0530

    1st commit

---
+++ fibonacci.py
@@ -0,0 +1,20 @@
+def generate_fibonacci(n):
+    """Return a list containing the first n Fibonacci numbers."""
+    if n <= 0:
+        return []
+    if n == 1:
+        return [0]
+
+    sequence = [0, 1]
+    while len(sequence) < n:
+        sequence.append(sequence[-1] + sequence[-2])
+    return sequence
+
+
+if __name__ == "__main__":
+    try:
+        count = int(input("How many Fibonacci numbers do you want? "))
+        fib_numbers = generate_fibonacci(count)
+        print("Fibonacci sequence:", fib_numbers)
+    except ValueError:
+        print("Please enter a valid integer.")
